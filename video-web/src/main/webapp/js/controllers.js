d784d62c3e4f4dd12ac7e5f79fb62cf4fab45adc
Mark Jerkovic <mjerkovic@crustyoldev.com>
2013-07-28 23:03:55 +1000

    Main menu styling and redirection after successful video creation.

--- video-web/src/main/webapp/js/controllers.js
+++ video-web/src/main/webapp/js/controllers.js
@@ -1,19 +1,8 @@
-function ContactsController($scope, $http) {
-    $http.get('/contacts').success(function(data) {
-        $scope.contacts = data.contacts;
-    });
-
-    $scope.postIt = function() {
-        alert("Heya!");
-    }
-
-}
-
-function NewVideoCtrl($scope, $http) {
+function NewVideoCtrl($scope, $http, $location) {
 
     $scope.submit = function() {
         $http.post('/perform/addVideo', $scope.data).success(function() {
-
+            $location.path("/videos");
         });
     }
 
