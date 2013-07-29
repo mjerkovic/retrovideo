11678be8cd9b46be984af0ab809a34abb38ac940
Mark Jerkovic <mjerkovic@crustyoldev.com>
2013-07-29 23:19:34 +1000

    Don't inject videoRepository into event handler but pass it into the handle method.

--- video-domain/src/main/java/com/mlj/retrovideo/domain/AddVideoEventHandler.java
+++ video-domain/src/main/java/com/mlj/retrovideo/domain/AddVideoEventHandler.java
@@ -1,17 +1,13 @@
 package com.mlj.retrovideo.domain;
 
 import org.axonframework.eventhandling.annotation.EventHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AddVideoEventHandler {
 
-    @Autowired
-    private JdbcVideoRepository videoRepository;
-
     @EventHandler
-    public void handle(VideoAddedEvent event) {
+    public void handle(VideoAddedEvent event, JdbcVideoRepository videoRepository) {
         videoRepository.addVideo(event);
     }
 
