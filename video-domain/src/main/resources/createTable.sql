108170413201b7fa11127ed7d0c650d8f55afd08
Mark Jerkovic <mjerkovic@crustyoldev.com>
2013-08-04 00:15:26 +1000

    Order videos by title and create a users table containing an admin in preparation for security.

--- video-domain/src/main/resources/createTable.sql
+++ video-domain/src/main/resources/createTable.sql
@@ -5,3 +5,13 @@ CREATE TABLE VIDEOS
   year INTEGER NOT NULL,
   duration INTEGER NOT NULL
 );
+
+CREATE TABLE USERS
+(
+  userName VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  firstName VARCHAR(30) NOT NULL,
+  lastName VARCHAR(50) NOT NULL
+);
+
+INSERT INTO USERS (userName, password, firstName, lastName) VALUES ('admin', 'admin', 'Administrator', '');
